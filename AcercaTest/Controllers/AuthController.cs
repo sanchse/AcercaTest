87630a8b0b6a3845f488272d45155a7da8548ebd
sanchse <sanchse@users.noreply.github.com>
2020-10-07 10:24:52 +0200

    fix

--- AcercaTest/Controllers/AuthController.cs
+++ AcercaTest/Controllers/AuthController.cs
@@ -9,8 +9,10 @@ using System.Net;
 using System.Threading;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace AcercaTest.Controllers {
+  [EnableCors(origins: "*", headers: "*", methods: "*")]
   [AllowAnonymous]
   [RoutePrefix("api/auth")]
   public class AuthController: ApiController {
